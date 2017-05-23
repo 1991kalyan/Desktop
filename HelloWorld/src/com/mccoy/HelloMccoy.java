e763291265c452688c5ee12bd86a2c75fea81f2e
mccoy <mccoy@mccoy-PC>
2017-05-23 15:31:53 +0530

    check in

---
+++ HelloWorld/src/com/mccoy/HelloMccoy.java
@@ -0,0 +1,14 @@
+package com.mccoy;
+
+public class HelloMccoy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Hello world");
+		
+	}
+
+}
